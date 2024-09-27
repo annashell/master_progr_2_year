8bff13694a1d439839ff92e36ba50b804b01300e
annashell <annaolegovna3@rambler.ru>
2024-09-28 00:25:14 +0300

    калькулятор без скобок и проверок на подряд идущие знаки

---
+++ scripts/text_gen/my_calculator.py
@@ -0,0 +1,107 @@
+def parse_string_to_numbers_and_symbols(string_to_calculate):
+    number = ""
+    numbers = []
+    for i in range(len(string_to_calculate)):
+        if i == 0:
+            if string_to_calculate[i].isdigit():
+                number += string_to_calculate[i]
+            elif string_to_calculate[i] == "-":
+                # первое отрицательное
+                number += string_to_calculate[i]
+            elif string_to_calculate[i] == "+":
+                pass
+            else:
+                print("ERROR", string_to_calculate[0], " cannot exist as the number")
+        else:
+            if string_to_calculate[i].isdigit():
+                number += string_to_calculate[i]
+            elif string_to_calculate[i] in "+-*/)(^":
+                numbers.append(int(number))
+                numbers.append(string_to_calculate[i])
+                number = ""
+            else:
+                print("ERROR", string_to_calculate[0], " cannot exist as the number")
+
+    if number != "":
+        numbers.append(number)
+
+    return numbers
+
+
+def process_degrees(symbols):
+    degree_positions = [pos for pos, symb in enumerate(symbols) if symb == '^']
+
+    # удаляем ^ и показатели степени
+    i = 0
+    while i < len(degree_positions):
+        pos = degree_positions[i] - i * 2
+        symbols[pos - 1] = int(symbols[pos - 1]) ** int(symbols[pos + 1])
+        symbols.pop(degree_positions[i] - i * 2)  # убираем ^
+        symbols.pop(degree_positions[i] - i * 2)  # убираем следующее число
+        i += 1
+    return symbols
+
+
+def process_brackets(parsed_symbols):
+    opening_brackets_pos = [pos for pos, symb in parsed_symbols if symb == '(']
+    closing_brackets_pos = [pos for pos, symb in parsed_symbols if symb == ')']
+    if len(opening_brackets_pos) != len(closing_brackets_pos):
+        raise Exception("Number of closing brackets is not equal to number of opening brackets")
+    return parsed_symbols
+
+
+def process_multiplication_and_division(symbols):
+    positions = []
+    for pos, symb in enumerate(symbols):
+        if symb == '*':
+            new_number = int(symbols[pos - 1]) * int(symbols[pos + 1])  # заменяем число перед * на результат умножения
+            symbols[pos - 1] = new_number
+            positions.append(pos)
+        if symb == '/':
+            if int(symbols[pos + 1]) == 0:
+                raise Exception("Division by zero!")
+            new_number = int(symbols[pos - 1]) / int(symbols[pos + 1])  # заменяем число перед / на результат деления
+            symbols[pos - 1] = new_number
+            positions.append(pos)
+
+    # удаляем * и / и последующее число
+    i = 0
+    while i < len(positions):
+        symbols.pop(positions[i] - i * 2)  # убираем
+        symbols.pop(positions[i] - i * 2)  # убираем следующее число
+        i += 1
+    return symbols
+
+
+def calculate_simplified_string(symbols):
+    positions = []
+    for pos, symb in enumerate(symbols):
+        if symb == '+':
+            new_number = int(symbols[pos - 1]) + int(symbols[pos + 1])  # заменяем число перед * на результат умножения
+            symbols[pos - 1] = new_number
+            positions.append(pos)
+        if symb == '-':
+            new_number = int(symbols[pos - 1]) - int(symbols[pos + 1])  # заменяем число перед / на результат деления
+            symbols[pos - 1] = new_number
+            positions.append(pos)
+
+    # удаляем * и / и последующее число
+    i = 0
+    while i < len(positions):
+        symbols.pop(positions[i] - i * 2)  # убираем
+        symbols.pop(positions[i] - i * 2)  # убираем следующее число
+        i += 1
+    return symbols
+
+
+def calculator(string_to_calculate: str):
+    parsed_symbols = parse_string_to_numbers_and_symbols(string_to_calculate)
+    parsed_symbols = process_degrees(parsed_symbols)
+    # parsed_symbols = process_brackets(parsed_symbols)
+    parsed_symbols = process_multiplication_and_division(parsed_symbols)
+    parsed_symbols = calculate_simplified_string(parsed_symbols)
+    return parsed_symbols[0]
+
+
+print(calculator("1+2^3^5"))
+print(1 + (2**3)**5)
