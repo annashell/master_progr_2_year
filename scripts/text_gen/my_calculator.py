db24053c991dcfa6c9b319a290c335ddccd2887e
annashell <annaolegovna3@rambler.ru>
2024-09-28 12:04:36 +0300

    исправила сложение и умножение

--- scripts/text_gen/my_calculator.py
+++ scripts/text_gen/my_calculator.py
@@ -47,61 +47,73 @@ def process_brackets(parsed_symbols):
     closing_brackets_pos = [pos for pos, symb in parsed_symbols if symb == ')']
     if len(opening_brackets_pos) != len(closing_brackets_pos):
         raise Exception("Number of closing brackets is not equal to number of opening brackets")
+
     return parsed_symbols
 
 
 def process_multiplication_and_division(symbols):
     positions = []
     for pos, symb in enumerate(symbols):
+        if symb in ('*', '/'):
+            positions.append(pos)
+
+    i = 0
+    while i < len(positions):
+        pos = positions[i] - i * 2
+        symb = symbols[pos]
         if symb == '*':
             new_number = int(symbols[pos - 1]) * int(symbols[pos + 1])  # заменяем число перед * на результат умножения
             symbols[pos - 1] = new_number
-            positions.append(pos)
         if symb == '/':
             if int(symbols[pos + 1]) == 0:
                 raise Exception("Division by zero!")
             new_number = int(symbols[pos - 1]) / int(symbols[pos + 1])  # заменяем число перед / на результат деления
             symbols[pos - 1] = new_number
-            positions.append(pos)
-
-    # удаляем * и / и последующее число
-    i = 0
-    while i < len(positions):
-        symbols.pop(positions[i] - i * 2)  # убираем
+        symbols.pop(positions[i] - i * 2)  # убираем * /
         symbols.pop(positions[i] - i * 2)  # убираем следующее число
         i += 1
+
     return symbols
 
 
 def calculate_simplified_string(symbols):
     positions = []
     for pos, symb in enumerate(symbols):
+        if symb in ('+', '-'):
+            positions.append(pos)
+
+    # удаляем + и - и последующее число
+    i = 0
+    while i < len(positions):
+        pos = positions[i] - i * 2
+        symb = symbols[pos]
         if symb == '+':
             new_number = int(symbols[pos - 1]) + int(symbols[pos + 1])  # заменяем число перед * на результат умножения
             symbols[pos - 1] = new_number
-            positions.append(pos)
         if symb == '-':
             new_number = int(symbols[pos - 1]) - int(symbols[pos + 1])  # заменяем число перед / на результат деления
             symbols[pos - 1] = new_number
-            positions.append(pos)
-
-    # удаляем * и / и последующее число
-    i = 0
-    while i < len(positions):
-        symbols.pop(positions[i] - i * 2)  # убираем
+        symbols.pop(positions[i] - i * 2)  # убираем + -
         symbols.pop(positions[i] - i * 2)  # убираем следующее число
         i += 1
+
     return symbols
 
 
 def calculator(string_to_calculate: str):
     parsed_symbols = parse_string_to_numbers_and_symbols(string_to_calculate)
-    parsed_symbols = process_degrees(parsed_symbols)
+    print(parsed_symbols)
+
     # parsed_symbols = process_brackets(parsed_symbols)
+    parsed_symbols = process_degrees(parsed_symbols)
+    print(parsed_symbols)
     parsed_symbols = process_multiplication_and_division(parsed_symbols)
+    print(parsed_symbols)
     parsed_symbols = calculate_simplified_string(parsed_symbols)
+    print(parsed_symbols)
+
     return parsed_symbols[0]
 
 
-print(calculator("1+2^3^5"))
-print(1 + (2**3)**5)
+print("result: ", calculator("1+2^3^5-15*5/5"))
+print(1 + (2 ** 3) ** 5 - 15 * 5 / 5)
